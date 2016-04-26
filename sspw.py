3d467a1a613badd004e6caf503a91316d728b9b7
Jin <305648262@qq.com>
2016-04-26 17:53:22 +0800

    V1.0

--- sspw.py
+++ sspw.py
@@ -1,7 +1,20 @@
 #!/usr/bin/env python3
 from urllib import request
-import json
-#z=抓取网页信息
+import json,os
+from collections import OrderedDict
+
+
+def updatePw(pw1,pw2,pw3,j):
+    length=len(j["configs"])
+    for x in range(length):
+        if ((j["configs"][x]["server"]).upper()=="US1.ISS.TF"):
+            j["configs"][x]["password"]=pw1
+        if ((j["configs"][x]["server"]).upper()=="HK2.ISS.TF"):
+            j["configs"][x]["password"]=pw2
+        if ((j["configs"][x]["server"]).upper()=="JP3.ISS.TF"):
+            j["configs"][x]["password"]=pw3
+
+# z=抓取网页信息
 with request.urlopen('http://www.ishadowsocks.net/') as f:
     data = f.read()
 # 从转成string格式
@@ -13,19 +26,24 @@ with request.urlopen('http://www.ishadowsocks.net/') as f:
     pw2=data[ind2+4:ind2+12]
     ind3=data.find("C密码:")
     pw3=data[ind3+4:ind3+12]
-    print(pw2+" "+pw3)
 try:
     # 定位密码
-    with open('C:\\Users\\Administrator\\Desktop\\gui-config.json', 'r') as r:
-        rcontent=r.read()
-        idx1=rcontent.find("US1.ISS.TF")
-        idx2=rcontent.find("HK2.ISS.TF")
-        idx3=rcontent.find("jp3.iss.tf")
-    with open('C:\\Users\\Administrator\\Desktop\\gui-config.json', 'w') as r:
-        # 定位密码
-        rcontent=rcontent.replace(rcontent[idx1+48:idx1+56],pw1)
-        rcontent=rcontent.replace(rcontent[idx2+49:idx2+57],pw2)
-        rcontent=rcontent.replace(rcontent[idx3+48:idx3+56],pw3)
-        r.write(rcontent)
+    if os.path.exists('./gui-config.json'):
+        with open('./gui-config.json', 'r') as r:
+            rcontent=r.read()
+            #转成json格式
+            j=json.loads(rcontent,object_pairs_hook=OrderedDict)
+            updatePw(pw1,pw2,pw3,j)
+        with open('./gui-config.json', 'w') as r:
+            r.write(json.dumps(j))
+    else:
+        with open('./gui-config.json', 'w') as r:
+            with open('./config.json', 'r') as r2:
+                content=r2.read()
+                jnew=json.loads(content,object_pairs_hook=OrderedDict)
+                updatePw(pw1,pw2,pw3,jnew)
+            r.write(json.dumps(jnew))
 except Exception as e:
     print (e)
+
+
